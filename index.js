22b31d33c6678de9a9088b3cc7f45f845b07d5bf
Lara Santos <laarasantos12@gmail.com>
2022-01-19 16:38:09 -0300

    Simple crud made in node for study

---
+++ index.js
@@ -0,0 +1,42 @@
+// config init
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// forma de ler json / middlewares
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// rotas de API
+const personRouter = require("./routes/personRoutes");
+
+app.use("/person", personRouter);
+
+// intial route / endpoint
+app.get("/", (req, res) => {
+  //mostrar req
+
+  res.json({ message: "Oi express!" });
+});
+
+// entregar uma porta
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.ont3y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Conectamos ao MongoDB!");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
